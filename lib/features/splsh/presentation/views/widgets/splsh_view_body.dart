804abd610d1bc756daafda6a2f1cfcf7c54a5414
Ahmed <ahmedmohameddx80@gmail.com>
2023-09-20 16:24:36 +0300

    first initial

---
+++ lib/features/splsh/presentation/views/widgets/splsh_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SplshViewBody extends StatelessWidget {
+  const SplshViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NewsApp());
+}
+
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
