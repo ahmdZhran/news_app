30666f0edc2f81484e8897f8814447c6f7323d2f
Ahmed <ahmedmohameddx80@gmail.com>
2023-09-26 02:15:15 +0300

    add black color to const and refactor color

--- lib/core/utlis/const_colors.dart
+++ lib/core/utlis/const_colors.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const kMyPinkColor = Color(0xfffBE4E1);
 const kMyGreyColor = Color(0xFFF1EFEF);
 const kMyWhiteColor = Color(0xFFFFFFFF);
+const kMyBlackColor = Color(0xff191717);
