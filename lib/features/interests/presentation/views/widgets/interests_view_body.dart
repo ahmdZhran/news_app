8455e7cfbe5b3df2644311489a2cd5b0a9e52672
Ahmed <ahmedmohameddx80@gmail.com>
2023-09-26 01:50:54 +0300

    refactor to select Item

--- lib/features/interests/presentation/views/widgets/interests_view_body.dart
+++ lib/features/interests/presentation/views/widgets/interests_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/core/utlis/text_style.dart';
 
+import 'select_item.dart';
+
 class IntersetsViewBody extends StatelessWidget {
   const IntersetsViewBody({super.key});
 
@@ -23,65 +25,6 @@ class IntersetsViewBody extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class SelectItem extends StatelessWidget {
-  const SelectItem({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          height: 65,
-          width: 65,
-          decoration: const BoxDecoration(
-            shape: BoxShape.circle,
-            color: Colors.white,
-          ),
-          child: Center(
-            child: Container(
-              height: 50,
-              width: 50,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(
-                  color: const Color(0xFFF1EFEF),
-                  width: 2,
-                ),
-              ),
-            ),
-          ),
-        ),
-        Expanded(
-          child: Container(
-            height: 60,
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(80),
-                topLeft: Radius.circular(80),
-                bottomRight: Radius.circular(80),
-                topRight: Radius.circular(80),
-              ),
-            ),
-            child: const Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: EdgeInsets.only(left: 20),
-                child: Text(
-                  'Health',
-                  style: StylesOfText.textStyle18,
-                ),
-              ),
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
---
+++ lib/features/interests/presentation/views/widgets/select_item.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utlis/text_style.dart';
+
+class SelectItem extends StatelessWidget {
+  const SelectItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          height: 65,
+          width: 65,
+          decoration: const BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.white,
+          ),
+          child: Center(
+            child: Container(
+              height: 50,
+              width: 50,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: const Color(0xFFF1EFEF),
+                  width: 2,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Container(
+            height: 60,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(80),
+                topLeft: Radius.circular(80),
+                bottomRight: Radius.circular(80),
+                topRight: Radius.circular(80),
+              ),
+            ),
+            child: const Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: EdgeInsets.only(left: 20),
+                child: Text(
+                  'Health',
+                  style: StylesOfText.textStyle18,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
