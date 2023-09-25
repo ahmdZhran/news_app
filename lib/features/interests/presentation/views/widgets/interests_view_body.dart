ef5a1719528930f7b2c1293c68c95282e3ef2088
Ahmed <ahmedmohameddx80@gmail.com>
2023-09-26 01:49:45 +0300

    select Item done

--- lib/features/interests/presentation/views/widgets/interests_view_body.dart
+++ lib/features/interests/presentation/views/widgets/interests_view_body.dart
@@ -6,58 +6,82 @@ class IntersetsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return const SafeArea(
       child: Padding(
-        padding: const EdgeInsets.only(left: 30, top: 10, right: 15),
+        padding: EdgeInsets.only(left: 30, top: 10, right: 15),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text(
+            Text(
               'Select Interests',
               style: StylesOfText.textStyle25,
             ),
-            const SizedBox(
+            SizedBox(
               height: 10,
             ),
-            Row(
-              children: [
-                Container(
-                  height: 60,
-                  width: 60,
-                  decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.white,
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    height: 60,
-                    decoration: const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(80),
-                        topLeft: Radius.circular(80),
-                        bottomRight: Radius.circular(80),
-                        topRight: Radius.circular(80),
-                      ),
-                    ),
-                    child: const Align(
-                      alignment: Alignment.centerLeft,
-                      child: Padding(
-                        padding: EdgeInsets.only(left: 20),
-                        child: Text(
-                          'Health',
-                          style: StylesOfText.textStyle18,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            )
+            SelectItem()
           ],
         ),
       ),
+    );
+  }
+}
+
+class SelectItem extends StatelessWidget {
+  const SelectItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          height: 65,
+          width: 65,
+          decoration: const BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.white,
+          ),
+          child: Center(
+            child: Container(
+              height: 50,
+              width: 50,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: const Color(0xFFF1EFEF),
+                  width: 2,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Container(
+            height: 60,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(80),
+                topLeft: Radius.circular(80),
+                bottomRight: Radius.circular(80),
+                topRight: Radius.circular(80),
+              ),
+            ),
+            child: const Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: EdgeInsets.only(left: 20),
+                child: Text(
+                  'Health',
+                  style: StylesOfText.textStyle18,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
