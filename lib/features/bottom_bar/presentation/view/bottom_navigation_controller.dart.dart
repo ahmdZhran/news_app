13754cdd53de32c9c8775ae7565c90c95262b214
Ahmed <ahmedmohameddx80@gmail.com>
2023-10-01 00:43:59 +0300

    delete unncessry code

--- lib/features/bottom_bar/presentation/view/bottom_navigation_controller.dart.dart
+++ lib/features/bottom_bar/presentation/view/bottom_navigation_controller.dart.dart
@@ -1,63 +1,4 @@
-// import 'package:flutter/material.dart';
-// import '../../../../core/models/icon_bottom_bar.dart';
-// import '../../../saved_item/presentation/view/saved_item.dart';
-// import '../../../search/presentation/views/search_view.dart';
-// import '../../../splsh/presentation/views/home/data/presentation/views/widgets/home_view_body.dart';
-
-// class BottomNavigationController extends StatefulWidget {
-//   const BottomNavigationController({Key? key}) : super(key: key);
-//   @override
-//   State<BottomNavigationController> createState() =>
-//       _BottomNavigationControllerState();
-// }
-
-// class _BottomNavigationControllerState
-//     extends State<BottomNavigationController> {
-//   int selectedIndex = 0;
-//   final List<Widget> screens = [
-//     const HomeViewBody(),
-//     const SearchView(),
-//     const SavedItem(),
-//   ];
-//   void _onTabSelected(int index) {
-//     setState(() {
-//       selectedIndex = index;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: screens[selectedIndex],
-//       bottomNavigationBar: BottomAppBar(
-//         color: Colors.orange,
-//         height: 80,
-//         child: Padding(
-//           padding: const EdgeInsets.symmetric(horizontal: 25),
-//           child: Row(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: [
-//               _buildBottomBarButton('Home', Icons.home, 0),
-//               _buildBottomBarButton('Saved', Icons.save, 1),
-//               _buildBottomBarButton('Search', Icons.search, 2),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-
-//   Widget _buildBottomBarButton(String text, IconData icon, int index) {
-//     return IconBottomBar(
-//       text: text,
-//       icon: icon,
-//       selected: selectedIndex == index,
-//       onPressed: () => _onTabSelected(index),
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
-
 import '../../../saved_item/presentation/view/saved_item.dart';
 import '../../../search/presentation/views/search_view.dart';
 import '../../../splsh/presentation/views/home/data/presentation/views/widgets/home_view_body.dart';
@@ -95,9 +36,8 @@ class _BottomNavigationControllerState
         child: BottomNavigationBar(
           backgroundColor: Colors.transparent,
           selectedItemColor: Colors.white,
-          // Color of the selected icon
-          unselectedItemColor: Colors.grey, // Color of unselected icons
-          elevation: 0, // Remove shadow
+          unselectedItemColor: Colors.grey,
+          elevation: 0,
           currentIndex: selectedIndex,
           onTap: _onTabSelected,
           items: [
@@ -116,7 +56,7 @@ class _BottomNavigationControllerState
         backgroundColor: Colors.white,
         child: Icon(
           icon,
-          color: Colors.black, // Color of the icon inside the circle
+          color: Colors.black,
         ),
       ),
       label: text,
