e69866966e8c5c3768563438aadbac1a63d9eedd
Ahmed <ahmedmohameddx80@gmail.com>
2023-10-02 22:41:23 +0300

    add animated text kit pakcage

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,6 @@ class NewsApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'News App',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: const SplshScreen(),
