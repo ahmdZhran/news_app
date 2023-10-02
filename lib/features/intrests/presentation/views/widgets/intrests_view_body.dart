da12863cbc5c8d3f5cefe40a70ad8772f840afeb
Ahmed <ahmedmohameddx80@gmail.com>
2023-10-03 01:09:43 +0300

    add list from intrestsModels

--- lib/features/intrests/presentation/views/widgets/intrests_view_body.dart
+++ lib/features/intrests/presentation/views/widgets/intrests_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/core/utlis/const_colors.dart';
+import 'package:news_app/models/intrests_models.dart';
 
 class IntrestsViewBody extends StatefulWidget {
   const IntrestsViewBody({super.key});
@@ -9,6 +10,7 @@ class IntrestsViewBody extends StatefulWidget {
 }
 
 class _IntrestsViewBodyState extends State<IntrestsViewBody> {
+  List<InterestsModels> interestsList = [];
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
