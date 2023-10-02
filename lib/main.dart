ef07fba67626ebe82bd51d403436cd0ef7c244cd
Ahmed <ahmedmohameddx80@gmail.com>
2023-10-02 22:35:15 +0300

    first intial

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'splsh_view/presentaion/view/splsh_view.dart';
+
+void main() {
+  runApp(const NewsApp());
+}
+
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'News App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SplshScreen(),
+    );
+  }
+}
---
+++ lib/splsh_view/presentaion/view/splsh_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'widgets/splsh_view_body.dart';
+
+class SplshScreen extends StatelessWidget {
+  const SplshScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SplshViewBody();
+  }
+}
