0567f3863dc952fc0950164ff8caa6c226f58cb6
Ahmed <ahmedmohameddx80@gmail.com>
2023-10-03 00:27:56 +0300

    add fonts and icon folders

--- lib/features/splsh_view/presentaion/view/widgets/splsh_view_body.dart
+++ lib/features/splsh_view/presentaion/view/widgets/splsh_view_body.dart
@@ -1,10 +1,66 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:news_app/core/utlis/app_router.dart';
+import 'package:news_app/core/widgets/custom_news_logo.dart';
 
-class SplshViewBody extends StatelessWidget {
+class SplshViewBody extends StatefulWidget {
   const SplshViewBody({super.key});
 
   @override
+  State<SplshViewBody> createState() => _SplshViewBodyState();
+}
+
+class _SplshViewBodyState extends State<SplshViewBody> {
+  @override
+  void initState() {
+    navigateToHomeView();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          children: [
+            const SizedBox(width: 20.0, height: 100.0),
+            const CustomNewsLogo(
+              width: 65,
+              hight: 65,
+              fontSize: 45,
+            ),
+            const SizedBox(width: 20.0, height: 100.0),
+            DefaultTextStyle(
+              style: const TextStyle(
+                fontSize: 40.0,
+                fontFamily: 'Horizon',
+              ),
+              child: AnimatedTextKit(
+                isRepeatingAnimation: false,
+                animatedTexts: [
+                  RotateAnimatedText(
+                    'News at Hand',
+                  ),
+                  RotateAnimatedText(
+                    'Top Headlines',
+                  ),
+                  RotateAnimatedText(
+                    'Latest Stories',
+                  ),
+                ],
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+  void navigateToHomeView() {
+    Future.delayed(const Duration(milliseconds: 8500), () {
+      GoRouter.of(context).push(AppRouter.kInterestsView);
+    });
   }
 }
