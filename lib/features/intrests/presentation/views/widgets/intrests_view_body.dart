b51f9744124ddc67696bc7b0386fd01a8729bb54
Ahmed <ahmedmohameddx80@gmail.com>
2023-10-03 01:08:55 +0300

    convert to statefull widget

--- lib/features/intrests/presentation/views/widgets/intrests_view_body.dart
+++ lib/features/intrests/presentation/views/widgets/intrests_view_body.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/core/utlis/const_colors.dart';
 
-class IntrestsViewBody extends StatelessWidget {
+class IntrestsViewBody extends StatefulWidget {
   const IntrestsViewBody({super.key});
 
+  @override
+  State<IntrestsViewBody> createState() => _IntrestsViewBodyState();
+}
+
+class _IntrestsViewBodyState extends State<IntrestsViewBody> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
