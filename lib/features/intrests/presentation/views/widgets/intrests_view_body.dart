fa72760b2cafea826d6c95a6276c811baa2c7a7b
Ahmed <ahmedmohameddx80@gmail.com>
2023-10-03 01:06:35 +0300

    set background color of interes view

--- lib/features/intrests/presentation/views/widgets/intrests_view_body.dart
+++ lib/features/intrests/presentation/views/widgets/intrests_view_body.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/core/utlis/const_colors.dart';
 
 class IntrestsViewBody extends StatelessWidget {
   const IntrestsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      backgroundColor: kMyPinkColor,
+    );
   }
 }
