bd1e19571afc75212702a3792a474e327d131046
Ahmed <ahmedmohameddx80@gmail.com>
2023-10-03 03:05:08 +0300

    selecte item done

--- lib/features/intrests/presentation/views/widgets/intrests_view_body.dart
+++ lib/features/intrests/presentation/views/widgets/intrests_view_body.dart
@@ -56,26 +56,38 @@ class _IntrestsViewBodyState extends State<IntrestsViewBody> {
                           IconButton(
                             onPressed: () {},
                             icon: Container(
-                              height: 65,
-                              width: 65,
-                              decoration: const BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: kMyWhiteColor,
-                              ),
-                              child: Center(
-                                child: Container(
-                                  height: 50,
-                                  width: 50,
-                                  decoration: BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    border: Border.all(
-                                      color: kMyGreyColor,
-                                      width: 2,
-                                    ),
-                                  ),
+                                height: 65,
+                                width: 65,
+                                decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: kMyWhiteColor,
                                 ),
-                              ),
-                            ),
+                                child: interestsList[index].isSelected
+                                    ? Center(
+                                        child: Container(
+                                          height: 50,
+                                          width: 50,
+                                          decoration: const BoxDecoration(
+                                            color: kMyBlackColor,
+                                            shape: BoxShape.circle,
+                                          ),
+                                          child: const Icon(Icons.check),
+                                        ),
+                                      )
+                                    : Center(
+                                        child: Container(
+                                          height: 50,
+                                          width: 50,
+                                          decoration: BoxDecoration(
+                                            shape: BoxShape.circle,
+                                            border: Border.all(
+                                              color: kMyGreyColor,
+                                              width: 2,
+                                            ),
+                                          ),
+                                        ),
+                                      )),
+                            //
                           ),
                           Expanded(
                             child: Container(
