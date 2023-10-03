62220ff29626e99a27363c15939731ac0403df6a
Ahmed <ahmedmohameddx80@gmail.com>
2023-10-03 04:27:16 +0300

    refactor to customIntrestsItem

--- lib/features/intrests/presentation/views/widgets/intrests_view_body.dart
+++ lib/features/intrests/presentation/views/widgets/intrests_view_body.dart
@@ -4,6 +4,7 @@ import 'package:news_app/core/utlis/app_router.dart';
 import 'package:news_app/core/utlis/const_colors.dart';
 import 'package:news_app/models/intrests_models.dart';
 import '../../../../../core/utlis/text_styles.dart';
+import 'custom_intrestes_item.dart';
 
 class IntrestsViewBody extends StatefulWidget {
   const IntrestsViewBody({super.key});
@@ -90,24 +91,9 @@ class _IntrestsViewBodyState extends State<IntrestsViewBody> {
                               ),
                             ),
                           ),
-                          Expanded(
-                            child: Container(
-                              height: 60,
-                              decoration: BoxDecoration(
-                                color: kMyWhiteColor,
-                                borderRadius: BorderRadius.circular(80),
-                              ),
-                              child: Align(
-                                alignment: Alignment.centerLeft,
-                                child: Padding(
-                                  padding: const EdgeInsets.only(left: 10.0),
-                                  child: Text(
-                                    interestsList[index].title,
-                                    style: const TextStyle(color: Colors.black),
-                                  ),
-                                ),
-                              ),
-                            ),
+                          CustomInterestItem(
+                            title: interestsList[index].title,
+                            backgroundColor: kMyWhiteColor,
                           ),
                         ],
                       ),
