1a2091b821a24cde7088b80f25699b5813e83aba
Ahmed <ahmedmohameddx80@gmail.com>
2023-10-07 22:07:26 +0300

    add parameters to InterstesView

--- lib/features/interests/presentation/views/interests_view.dart
+++ lib/features/interests/presentation/views/interests_view.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/core/models/titels_model.dart';
 import 'package:news_app/core/utlis/const_colors.dart';
 
 import 'widgets/interests_view_body.dart';
 
 class InterstesView extends StatelessWidget {
-  const InterstesView({super.key});
-
+  const InterstesView({super.key, required this.interestsTitles});
+  final InterestsTitles interestsTitles;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: kMyPinkColor,
-      body: IntersetsViewBody(),
+      body: IntersetsViewBody(
+        interestTitels: interestsTitles,
+      ),
     );
   }
 }
